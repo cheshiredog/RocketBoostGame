87b44ee778129fb11d9fe53b7d806bb56ca50502
LadyMadeline <cheshiredog1865@gmail.com>
2023-01-31 21:50:19 +0200

    Movement added

--- RocketBoostGame/Assets/Scripts/Movement.cs
+++ RocketBoostGame/Assets/Scripts/Movement.cs
@@ -4,37 +4,60 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody myRigidbody;
+    Transform myTransform;
+    // Rocket speed variable
+    [SerializeField] float pushSpeed = 100f;
+    // Rocket rotation speed variable
+    [SerializeField] float rotateSpeed = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Getting access to the rigidbody
+        myRigidbody = GetComponent<Rigidbody>();
+        // Getting access to the transform
+        myTransform= GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Pushing rocket
         ProcessThrust();
+        // Rotating rocket
         ProcessRotation();
     }
 
     void ProcessThrust()
     {
+        // Checking if space is pressed
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Pressed space");
+            // Pushing rocket upwards by y-axis, if space is pressed
+            myRigidbody.AddRelativeForce(pushSpeed * Time.deltaTime * Vector3.up);
         }
     }
 
     void ProcessRotation()
     {
+        // Checking if A key is pressed
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Rotating Left");
+            // Rotating rocket to the left
+            ApplyRotation(rotateSpeed);
         }
-
+        // Checking if D key is pressed
         else if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Rotating Right");
+            // Rotating rocket to the right
+            ApplyRotation(-rotateSpeed);
         }
+    }
+
+    void ApplyRotation(float rotation)
+    {
+        // Rotating rocket
+        myTransform.Rotate(rotation * Time.deltaTime * Vector3.forward);
     }
 }
